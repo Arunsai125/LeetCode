3875c0ae490badb76d23c69fee8b0ed672c0ccb1
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-05-04 20:19:05 -0400

    Time: 0 ms (100%), Space: 40.7 MB (39.16%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.java
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.java
@@ -1,18 +1,17 @@
 class Solution {
     public int numTilings(int n) {
-        if(n <= 2)
-            return n;
-        int mod = 1_000_000_007;
-        long[] fullTiling = new long[n+1];
-        long[] partTiling = new long[n+1];
-        fullTiling[1] = 1;
-        fullTiling[2] = 2; 
-        partTiling[1] = 0; 
-        partTiling[2] = 1; 
-        for(int i=3; i <= n; i++){
-            fullTiling[i] = (fullTiling[i-1] + fullTiling[i-2] + 2 * partTiling[i-1]) % mod;
-            partTiling[i] = (partTiling[i-1] + fullTiling[i-2]) % mod;
-        }        
-        return (int)(fullTiling[n]);
+        int dp[] = new int[n+4];
+        
+        int mod = (int)(Math.pow(10,9)+7);
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 5;
+        
+        for(int i=4;i<=n;i++){
+            dp[i] = ((2*dp[i-1])%mod)+dp[i-3];
+            dp[i] %= mod;
+        }
+        
+        return dp[n];
     }
 }
