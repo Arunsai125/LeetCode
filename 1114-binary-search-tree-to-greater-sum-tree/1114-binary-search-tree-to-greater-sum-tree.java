d805ef530a496ad72d291b255d72db98f029efe0
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-05-06 22:32:24 -0400

    Time: 0 ms (100%), Space: 41.2 MB (56.63%) - LeetHub

--- 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.java
+++ 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.java
@@ -1,36 +1,14 @@
 class Solution {
+    int sum=0;
     public TreeNode bstToGst(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            int k = q.size();
-            for(int i=0;i<k;i++){
-                TreeNode temp = q.poll();
-                list.add(temp.val);
-                if(temp.left!=null) q.add(temp.left);
-                if(temp.right!=null) q.add(temp.right);
-            }
-        }
-        Collections.sort(list);
-        q.add(root);
-        while(!q.isEmpty()){
-            int k = q.size();
-            for(int i=0;i<k;i++){
-                TreeNode temp = q.poll();
-                temp.val = temp.val + findValue(temp.val,list);
-                if(temp.left!=null) q.add(temp.left);
-                if(temp.right!=null) q.add(temp.right);
-            }
-        }
-    return root;    
+        inorder(root);
+    return root;
     }
-    public int findValue(int num, List<Integer> list){
-        int ans=0;
-        int k = list.indexOf(num);
-        for(int i=k+1;i<list.size();i++){
-            ans += list.get(i);
-        }
-    return ans;
-    } 
+    public void inorder(TreeNode root){
+        if(root==null) return;
+        inorder(root.right);
+        sum += root.val;
+        root.val = sum;
+        inorder(root.left);
+    }
 }
