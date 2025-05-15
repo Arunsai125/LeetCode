396a5aae3dadebe719c578ab478f9c5ea37f313d
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-05-15 19:11:27 -0400

    Time: 50 ms (16.19%), Space: 45.3 MB (14.15%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,34 +1,52 @@
+class Pair{
+    int target;
+    int weight;
+    public Pair(int target, int weight){
+        this.target=target; 
+        this.weight=weight;
+    }
+}
 class Solution {
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        int ans=n;
-        int c=n;
-            int[][] matrix = new int[n][n];
-            for(int i=0;i<n;i++){
-                Arrays.fill(matrix[i],(int)1e9);
-                matrix[i][i]=0;
+        int ans=-1;
+        Map<Integer,List<Pair>> map = new HashMap<>();
+        for(int i=0;i<n;i++) map.put(i, new ArrayList<Pair>());
+        for(int[] edge : edges){
+            map.get(edge[0]).add(new Pair(edge[1],edge[2]));
+            map.get(edge[1]).add(new Pair(edge[0],edge[2]));
+        }
+        int maxNeighbours = n-1;
+        for(int src=0;src<n;src++){
+            int temp = DijkstraAlgo(src,map,distanceThreshold,n);
+            if(temp<=maxNeighbours){
+                ans = src;
+                maxNeighbours = temp;
             }
-            for(int edge[] : edges){
-                matrix[edge[0]][edge[1]] = edge[2];
-                matrix[edge[1]][edge[0]] = edge[2];
-            }
-            for(int src=0;src<n;src++){
-                for(int i=0;i<n;i++){
-                    for(int j=0;j<n;j++){
-                        matrix[i][j] = Math.min(matrix[i][j], matrix[i][src] + matrix[src][j]);
-                    }
-                }
-            }
-            for(int src=0;src<n;src++){
-                int temp=0;
-                for(int i=0;i<n;i++){
-                    if(src==i) continue;
-                    if(matrix[src][i] <= distanceThreshold) temp++;
-                }
-                if(temp<=c){
-                    c=temp;
-                    ans=src;
-                }
-            }
+        }
     return ans;
+    }
+    public int DijkstraAlgo(int src, Map<Integer,List<Pair>> map, int k, int n){
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.weight-b.weight);
+        int[] dist = new int[n];
+        Arrays.fill(dist,(int)1e9);
+        dist[src]=0;
+        pq.add(new Pair(src,0));
+        while(!pq.isEmpty()){
+            Pair temp = pq.poll();
+            for(Pair p : map.get(temp.target)){
+                int node = p.target;
+                int dis = p.weight;
+                if(dist[temp.target] + dis < dist[node]){
+                    dist[node] = dist[temp.target] + dis;
+                    pq.add(new Pair(node, dist[node]));
+                }
+            }
+        }
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(i==src) continue;
+            if(dist[i] <= k) c++;
+        }
+    return c;
     }
 }
