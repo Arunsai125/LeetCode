c7b5d9e5ca155bcae5eab7c2b44db80cf166c93c
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-05-19 18:26:53 -0400

    Time: 0 ms (100%), Space: 43 MB (72.05%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
@@ -2,17 +2,12 @@ public class Codec {
 
     private static String origionalUrl;
     private static String tinyUrl;
-    // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
         origionalUrl=longUrl;
         tinyUrl="http://tinyurl.com/4e9iAkl";
         return tinyUrl;
     }
-
-    // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-
-
         return origionalUrl;
     }
 }
