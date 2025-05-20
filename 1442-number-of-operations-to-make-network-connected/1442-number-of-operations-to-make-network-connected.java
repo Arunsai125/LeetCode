33219069b742da83218e005d5f34babe9ad49510
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-05-20 19:05:47 -0400

    Time: 16 ms (27.71%), Space: 61.2 MB (67.08%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.java
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.java
@@ -1,34 +1,46 @@
+class DisjointSet{
+    List<Integer> parent;
+    List<Integer> size;
+    public DisjointSet(int n){
+        parent = new ArrayList<>();
+        size = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            parent.add(i);
+            size.add(1);
+        }
+    }
+    public int findParent(int node){
+        if(node==parent.get(node)) return node;
+        int par = findParent(parent.get(node));
+        parent.set(node, par);
+    return parent.get(node);
+    }
+    public void unionBySize(int a, int b){
+        int upa = findParent(a);
+        int upb = findParent(b);
+        if(upa==upb) return;
+        if(size.get(upa) < size.get(upb)){
+            parent.set(upa, upb);
+            size.set(upb, size.get(upa) + size.get(upb));
+        }
+        else{
+            parent.set(upb, upa);
+            size.set(upa, size.get(upb) + size.get(upa));
+        }
+    }
+}
+
 class Solution {
     public int makeConnected(int n, int[][] connections) {
         if(connections.length < n-1) return -1;
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+        int ans=0;
+        DisjointSet ds = new DisjointSet(n);
         for(int[] con : connections){
-            adj.get(con[0]).add(con[1]);
-            adj.get(con[1]).add(con[0]);
+            ds.unionBySize(con[0],con[1]);
         }
-        boolean[] visited = new boolean[n];
-        int totalCompo = 0;
         for(int i=0;i<n;i++){
-            if(visited[i]==false){
-                totalCompo++;
-                visited[i]=true;
-                bfs(adj,i,visited);
-            }
+            if(ds.findParent(i)==i) ans++;
         }
-    return totalCompo-1;
-    }
-    public void bfs(List<List<Integer>> adj, int node, boolean[] visited){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(node);
-        while(!q.isEmpty()){
-            int temp = q.poll();
-            for(int val : adj.get(temp)){
-                if(visited[val]==false){
-                    visited[val]=true;
-                    q.add(val);
-                }
-            }
-        }
+    return ans-1;
     }
 }
