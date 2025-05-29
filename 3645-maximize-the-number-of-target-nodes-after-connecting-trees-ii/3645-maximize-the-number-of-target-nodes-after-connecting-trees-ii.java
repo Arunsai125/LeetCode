75c426cf7d375c5555695b6a97a651bda4a28b29
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-05-28 21:19:25 -0400

    Time: 109 ms (74.58%), Space: 116.7 MB (76.27%) - LeetHub

--- 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.java
+++ 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.java
@@ -2,8 +2,6 @@ class Solution {
     public int[] maxTargetNodes(int[][] edges1, int[][] edges2) {
         int n = edges1.length + 1;
         int m = edges2.length + 1;
-
-        // build adjacency
         List<List<Integer>> adj1 = new ArrayList<>();
         List<List<Integer>> adj2 = new ArrayList<>();
         for(int i = 0; i < n; i++) adj1.add(new ArrayList<>());
@@ -16,31 +14,21 @@ class Solution {
             adj2.get(e[0]).add(e[1]);
             adj2.get(e[1]).add(e[0]);
         }
-
-        // BFS-once to get depths + parity counts for Tree1
         int[] depth1 = new int[n];
         int[] d1Counts = bfsDepthAndCount(adj1, depth1);
         int even1 = d1Counts[0], odd1 = d1Counts[1];
-
-        // BFS-once to get depths + parity counts for Tree2
         int[] depth2 = new int[m];
         int[] d2Counts = bfsDepthAndCount(adj2, depth2);
         int even2 = d2Counts[0], odd2 = d2Counts[1];
-
-        // best extra targets from Tree2
         int best2 = Math.max(even2, odd2);
 
-        // build answer
         int[] ans = new int[n];
         for(int i = 0; i < n; i++) {
-            // in-Tree1 targets = size of same-parity group
             int sameParityCount = (depth1[i] % 2 == 0 ? even1 : odd1);
             ans[i] = sameParityCount + best2;
         }
         return ans;
     }
-
-    // returns [evenCount, oddCount]
     private int[] bfsDepthAndCount(List<List<Integer>> adj, int[] depth) {
         int n = adj.size();
         boolean[] vis = new boolean[n];
