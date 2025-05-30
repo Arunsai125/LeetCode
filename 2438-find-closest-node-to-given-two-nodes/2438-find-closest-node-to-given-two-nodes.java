df7a9ff184030069f3c32995d30549ff1e74a67a
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-05-29 21:29:31 -0400

    Time: 10 ms (98.94%), Space: 56.9 MB (93.64%) - LeetHub

--- 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.java
+++ 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.java
@@ -1,37 +1,30 @@
 class Solution {
     public int closestMeetingNode(int[] edges, int node1, int node2) {
         int n = edges.length;
-        int[] dist1 = new int[n];
-        int[] dist2 = new int[n];
+        int dist1[] = new int[n];
+        int dist2[] = new int[n];
         Arrays.fill(dist1, -1);
         Arrays.fill(dist2, -1);
-
-        // Fill distance from node1
-        fillDistances(edges, node1, dist1);
-        // Fill distance from node2
-        fillDistances(edges, node2, dist2);
-
-        int minDist = Integer.MAX_VALUE;
-        int result = -1;
-
-        for (int i = 0; i < n; i++) {
-            if (dist1[i] != -1 && dist2[i] != -1) {
-                int maxDist = Math.max(dist1[i], dist2[i]);
-                if (maxDist < minDist) {
-                    minDist = maxDist;
-                    result = i;
+        traverseNode(edges, dist1, node1);
+        traverseNode(edges, dist2, node2);
+        int maxDis = Integer.MAX_VALUE;
+        int ans = -1;
+        for(int i=0;i<n;i++){
+            if(dist1[i]!=-1 && dist2[i]!=-1){
+                int tempDis = Math.max(dist1[i],dist2[i]);
+                if(tempDis < maxDis){
+                    ans = i;
+                    maxDis = tempDis;
                 }
             }
         }
-
-        return result;
+    return ans;
     }
-
-    private void fillDistances(int[] edges, int start, int[] dist) {
+    public void traverseNode(int[] edge, int[] dist, int start){
         int d = 0;
-        while (start != -1 && dist[start] == -1) {
+        while(start!=-1 && dist[start]==-1){
             dist[start] = d++;
-            start = edges[start];
+            start = edge[start];
         }
     }
 }
