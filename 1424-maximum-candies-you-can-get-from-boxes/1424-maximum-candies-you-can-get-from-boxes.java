ee6a147e20161564de12683e6a9e911f04602fb5
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-06-03 07:19:21 -0400

    Time: 2 ms (98.51%), Space: 57.4 MB (48.76%) - LeetHub

--- 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.java
+++ 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.java
@@ -1,32 +1,24 @@
 class Solution {
-        public int maxCandies(int[] status,int[] candies,int[][] keys,int[][] containedBoxes,int[] initialBoxes) {
-
-                int count=0; // Total candies collected
-                        boolean[] vis=new boolean[status.length]; // Track visited boxes
-                                for(int v:initialBoxes){
-                                            dfs(v,status,keys,containedBoxes,vis);
-                                                    }
-
-                                                            for(int i=0;i<candies.length;i++){
-                                                                        if(vis[i]&&status[i]==1){
-                                                                                        count+=candies[i];
-                                                                                                    }
-                                                                                                            }
-                                                                                                                    return count;
-                                                                                                                        }
-
-                                                                                                                            public void dfs(int v,int[] status,int[][] keys,int[][] containedBoxes,boolean[] vis){ 
-
-                                                                                                                                    vis[v]=true; // Mark the current box as visited
-                                                                                                                                            for(int vKey:keys[v]){
-                                                                                                                                                        if(vKey==v) continue; // Skip self-key
-                                                                                                                                                                    status[vKey]=1; // Unlock the box
-                                                                                                                                                                            }
-
-                                                                                                                                                                                    for(int vContained:containedBoxes[v]){
-                                                                                                                                                                                                if(!vis[vContained]){
-                                                                                                                                                                                                                dfs(vContained,status,keys,containedBoxes,vis);
-                                                                                                                                                                                                                            }
-                                                                                                                                                                                                                                    }
-                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                        }
+    public int maxCandies(int[] vis, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
+        boolean foundOpenable = true;
+        int totalCandies = 0;
+        List<Integer> boxes = new ArrayList<>();
+        for (int b : initialBoxes) boxes.add(b);
+        while (!boxes.isEmpty() && foundOpenable) {
+            foundOpenable = false;
+            List<Integer> nextBoxes = new ArrayList<>();
+            for (int boxId : boxes) {
+                if (vis[boxId] == 1) {
+                    foundOpenable = true;
+                    for (int k : containedBoxes[boxId]) nextBoxes.add(k);
+                    for (int keyId : keys[boxId]) vis[keyId] = 1;
+                    totalCandies += candies[boxId];
+                } else {
+                    nextBoxes.add(boxId);
+                }
+            }
+            boxes = nextBoxes;
+        }
+        return totalCandies;
+    }
+}
