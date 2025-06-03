89c6c7efe9c738c0cf414ced1ceb49922b942a9a
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-06-03 19:47:44 -0400

    Time: 8 ms (56.43%), Space: 49.4 MB (58.19%) - LeetHub

---
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -0,0 +1,18 @@
+class NumArray {
+    List<Integer> prefixSum;
+    public NumArray(int[] nums) {
+        prefixSum = new ArrayList<>();
+        int sum = 0;
+        for(int i : nums){
+            prefixSum.add(sum);
+            sum += i;
+        }
+        prefixSum.add(sum);
+    }
+    
+    public int sumRange(int left, int right) {
+        if(left==right) return prefixSum.get(right+1) - prefixSum.get(right);
+        if(left==0) return prefixSum.get(right+1);
+     return prefixSum.get(right+1) - prefixSum.get(left);
+    }
+}
