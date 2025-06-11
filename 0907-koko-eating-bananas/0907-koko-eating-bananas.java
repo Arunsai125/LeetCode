12fa1f9a2fc84e2a3ecfc2f6c46bf8fcb3f454cd
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-06-10 20:46:53 -0400

    Time: 18 ms (52.55%), Space: 44.9 MB (88.35%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        long start = 1;
+        long end = 0;
+        long ans=-1;
+        for(int i : piles){
+            end += (long)i;
+        }
+        while(start<=end){
+            long mid = start + (end-start)/2;
+            if(isPossible(piles,h,mid)){
+                ans = mid;
+                end = mid-1; 
+            }
+            else start = mid+1;
+        }
+    return (int)ans;
+    }
+    public boolean isPossible(int[] nums, int target, long val){
+        long ans=0;
+        for(int i : nums){
+            if(i%val==0) ans += (long)i/val;
+            else ans += (long)i/val + 1;
+        }
+    return ans<=target;
+    }
+}
