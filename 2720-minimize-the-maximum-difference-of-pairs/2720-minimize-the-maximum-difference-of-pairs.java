446d37d14a25029387c7975a364c0f935d425324
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-06-13 10:59:19 -0400

    Time: 18 ms (40%), Space: 56.8 MB (7.5%) - LeetHub

--- 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.java
+++ 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.java
@@ -1,19 +1,27 @@
 class Solution {
     public int minimizeMax(int[] nums, int p) {
-        if (p == 0) return 0;
         Arrays.sort(nums);
-        int n = nums.length, left = 0, right = nums[n-1] - nums[0];
-        while (left < right) {
-            int mid = left + (right - left) / 2, pairs = 0;
-            for (int i = 1; i < n; i++) {
-                if (nums[i] - nums[i-1] <= mid) {
-                    pairs++;
-                    i++;
-                }
+        int ans=-1;
+        int start = 0;
+        int end = nums[nums.length-1] - nums[0];
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(canMakePairs(nums,p,mid)){
+                ans = mid;
+                end = mid-1;
             }
-            if (pairs >= p) right = mid;
-            else left = mid + 1;
+            else start = mid+1;
         }
-        return left;
+    return ans;
+    }
+    public boolean canMakePairs(int[] nums, int target, int curr){
+        int pairs=0;
+        for(int i=0;i<nums.length;i++){
+            if(i+1<nums.length && nums[i+1] - nums[i] <= curr){
+                pairs++;
+                i++;
+            }
+        }
+    return pairs >= target;
     }
 }
