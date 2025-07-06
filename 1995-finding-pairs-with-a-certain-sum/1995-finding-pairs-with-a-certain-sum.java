633455e33667e78a528c1969fca6e2a3992da585
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-07-05 20:32:44 -0400

    Time: 153 ms (47.72%), Space: 70.4 MB (99.3%) - LeetHub

--- 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.java
+++ 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.java
@@ -1,30 +1,27 @@
 class FindSumPairs {
     private int[] nums1;
     private int[] nums2;
-    private Map<Integer,Integer> map1;
-    private Map<Integer,Integer> map2;
+    private Map<Integer,Integer> map;
     public FindSumPairs(int[] nums1, int[] nums2) {
         this.nums1=nums1;
         this.nums2=nums2;
-        map1 = new HashMap<>();
-        map2 = new HashMap<>();
-        for(int i : nums1) map1.put(i, map1.getOrDefault(i,0)+1);
-        for(int i : nums2) map2.put(i, map2.getOrDefault(i,0)+1);
+        map = new HashMap<>();
+        for(int i : nums2) map.put(i, map.getOrDefault(i,0)+1);
     }
     
     public void add(int index, int val) {
         int newVal = nums2[index] + val;
         int oldVal = nums2[index];
-        map2.put(oldVal, map2.get(oldVal)-1);
-        map2.put(newVal, map2.getOrDefault(newVal,0)+1);
+        map.put(oldVal, map.get(oldVal)-1);
+        map.put(newVal, map.getOrDefault(newVal,0)+1);
         nums2[index] += val;
     }
     
     public int count(int tot) {
         int ans=0;
-        for(int i : map1.keySet()){
+        for(int i : nums1){
             int reqVal = tot-i;
-            if(map2.containsKey(reqVal)) ans += map2.get(reqVal) * map1.get(i);
+            if(map.containsKey(reqVal)) ans += map.get(reqVal);
         }
     return ans;
     }
