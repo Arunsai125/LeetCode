fbf8648f0c064ab8d7f7cf06ddefd6ca4d58339d
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-07-08 19:27:41 -0400

    Time: 6 ms (11.72%), Space: 46 MB (84.58%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,26 +1,12 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-     int n1=nums1.length;
-     int n2=nums2.length;
-    if(n1>n2) return findMedianSortedArrays(nums2,nums1);
-    int k = n1+n2;
-    int ele = (k+1)/2;
-    int s=0,e=n1;
-    while(s<=e){
-        int m1 = (s+e)/2;
-        int m2=ele-m1;
-        int l1 = (m1>0) ? nums1[m1-1] : Integer.MIN_VALUE;
-            int l2 = (m2>0) ? nums2[m2-1] : Integer.MIN_VALUE;
-            int r1 = (m1<n1) ? nums1[m1] : Integer.MAX_VALUE;
-            int r2 = (m2<n2) ? nums2[m2] : Integer.MAX_VALUE;
-        if(l1<=r2 && l2<=r1){
-            if(k%2==0)
-                    return (double) ((Math.max(l1,l2)+Math.min(r1,r2))/2.0);
-            else return (double) Math.max(l1,l2);
+        List<Integer> list = new ArrayList<>();
+        for(int i :  nums1) list.add(i);
+        for(int i : nums2) list.add(i);
+        Collections.sort(list);
+        if(list.size()%2==0){
+            return ((double)list.get(list.size()/2) + (double)list.get(list.size()/2 - 1)) / 2.0;
         }
-        else if(l1>r2) e=m1-1;
-        else s=m1+1;
-    }
-    return 0;
+    return (double) list.get(list.size()/2);
     }
 }
