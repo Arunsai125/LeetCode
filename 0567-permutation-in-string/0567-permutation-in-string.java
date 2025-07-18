6a7d82e42a9c479a3c6a7b91df57d69f7cdfc50b
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-07-18 15:13:53 -0400

    Time: 8 ms (60.81%), Space: 42.8 MB (68.29%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,13 +1,15 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length()) return false;
         int[] arr1 = new int[26];
-        for(int i=0;i<s1.length();i++) arr1[s1.charAt(i)-'a']++;
-        for(int i=0;i<=s2.length()-s1.length();i++){
-            int[] arr2 = new int[26];
-            for(int j=i;j<i+s1.length();j++) arr2[s2.charAt(j)-'a']++;
+        int[] arr2 = new int[26];
+        for(int i=0;i<s1.length();i++) { arr1[s1.charAt(i)-'a']++; arr2[s2.charAt(i)-'a']++; }
+        for(int i=0;i<s2.length()-s1.length();i++){
             if(areArraysEqual(arr1,arr2)) return true;
+            arr2[s2.charAt(i)-'a']--;
+            arr2[s2.charAt(i + s1.length())-'a']++;
         }
-    return false;
+    return areArraysEqual(arr1,arr2);
     }
     public boolean areArraysEqual(int[] arr1, int[] arr2){
         for(int i=0;i<26;i++){
