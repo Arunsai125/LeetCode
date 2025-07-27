395adb502d0d1468df0e753723b0020a8322ea42
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-07-26 20:45:11 -0400

    Time: 0 ms (100%), Space: 42.1 MB (13.73%) - LeetHub

--- 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.java
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.java
@@ -1,15 +1,15 @@
 class Solution {
     public int countHillValley(int[] nums) {
-        int ctn = 0;
-
-        for(int i = 1, last = nums[0]; i < nums.length - 1; i++){
-            if(nums[i] != nums[i + 1]){
-                if( (last > nums[i] && nums[i] < nums[i + 1]) ||
-                    (last < nums[i] && nums[i] > nums[i + 1]) ) ctn++;
-                last = nums[i];
+        int ans=0;
+        int prev = nums[0];
+        for(int i=1;i<nums.length-1;i++){
+            if(nums[i]!=nums[i+1]){
+                if( (nums[i]>prev && nums[i]>nums[i+1]) || (nums[i]<prev && nums[i]<nums[i+1]) ){
+                    ans++;
+                    prev = nums[i];
+                }
             }
         }
-
-        return ctn;
+    return ans;
     }
 }
