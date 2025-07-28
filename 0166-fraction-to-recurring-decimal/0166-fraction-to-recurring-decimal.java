0451e1e7679d436d861002a19a3eeae924711ff7
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-07-28 19:39:59 -0400

    Time: 1 ms (99.87%), Space: 40.7 MB (97.68%) - LeetHub

--- 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.java
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.java
@@ -1,36 +1,36 @@
 class Solution {
     public String fractionToDecimal(int numerator, int denominator) {
-        boolean isNegative = ((numerator>0 && denominator<0) || (denominator>0 && numerator<0)) ? true : false;
         StringBuilder ans = new StringBuilder();
+        boolean isNegative = ( (numerator>0 && denominator<0) || (numerator<0 && denominator>0) ) ? true : false;
         long num = Math.abs((long)numerator);
         long den = Math.abs((long)denominator);
         long q = num/den;
         ans.append(q);
-        long r = num%den;
-        if(r==0) {
+        long rem = num%den;
+        if(rem==0){
             if(isNegative) ans.insert(0, "-");
-            return ans.toString();
+        return ans.toString();
         }
         else{
             ans.append(".");
             Map<Long,Integer> map = new HashMap<>();
-            while(r!=0){
-                if(map.containsKey(r)){
-                    int index = map.get(r);
+            while(rem!=0){
+                if(map.containsKey(rem)){
+                    int index = map.get(rem);
                     ans.insert(index, "(");
                     ans.append(")");
                     break;
                 }
                 else{
-                    map.put((long)r,ans.length());
-                    r = r*10;
-                    q = r/den;
-                    r = r%den;
+                    map.put(rem, ans.length());
+                    rem = rem*10;
+                    q = rem/den;
                     ans.append(q);
+                    rem = rem%den;
                 }
             }
         }
-    if(isNegative) ans.insert(0, "-");
+         if(isNegative) ans.insert(0, "-");
     return ans.toString();
     }
 }
