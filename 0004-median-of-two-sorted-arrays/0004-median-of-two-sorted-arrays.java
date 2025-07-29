c4f4fcb3b0c1c7de0985cc59650aa4715bb7e500
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-07-28 20:20:41 -0400

    Time: 1 ms (100%), Space: 45.9 MB (93.06%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,31 +1,26 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        List<Integer> list = new ArrayList<>();
-        int p1 = 0;
-        int p2 = 0;
-        while(p1<nums1.length && p2<nums2.length){
-            if(nums1[p1] < nums2[p2]){
-                list.add(nums1[p1]);
-                p1++;
+        int length = nums1.length + nums2.length;
+        int leftHalf = (length+1) / 2;
+        if(nums1.length>nums2.length) return findMedianSortedArrays(nums2, nums1);
+        int start=0;
+        int end = nums1.length;
+        while(start<=end){
+            int mid1 = (start + end)/2;
+            int mid2 = leftHalf - mid1;
+            int l1 = (mid1-1>=0) ? nums1[mid1-1] : Integer.MIN_VALUE;
+            int l2 = (mid2-1>=0) ? nums2[mid2-1] : Integer.MIN_VALUE;
+            int r1 = (mid1<nums1.length) ? nums1[mid1] : Integer.MAX_VALUE;
+            int r2 = (mid2<nums2.length) ? nums2[mid2] : Integer.MAX_VALUE;
+            if(l1<=r2 && l2<=r1){
+                if(length%2==0){
+                    return ((double)(Math.max(l1,l2)) + (double) (Math.min(r1,r2)) ) /2.0;
+                }
+                else return (double) Math.max(l1,l2);
             }
-            else{
-                list.add(nums2[p2]);
-                p2++;
-            }
+            else if(l1>r2) end = mid1-1; 
+            else start = mid1+1;
         }
-        while(p1<nums1.length){
-            list.add(nums1[p1]);
-                p1++;
-        }
-        while(p2<nums2.length){
-            list.add(nums2[p2]);
-                p2++;
-        }
-        if(list.size()%2==0){
-            double e1 = (double) list.get(list.size()/2);
-            double e2 = (double) list.get(list.size()/2 - 1);
-            return (e1 + e2) / 2.0;
-        }
-    return (double) list.get(list.size()/2);
+    return 0.0;
     }
 }
