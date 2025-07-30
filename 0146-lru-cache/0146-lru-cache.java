e8f552a01f2c6c5e797b1f22539bbf6c57634270
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-07-30 07:57:02 -0400

    Time: 47 ms (52.97%), Space: 113.2 MB (66.25%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,24 +1,25 @@
 class Node{
     int key;
     int value;
-    Node next;
     Node prev;
+    Node next;
     public Node(int key, int value){
-        this.key = key;
-        this.value = value;
-        next=null;
+        this.key=key;
+        this.value=value;
         prev=null;
+        next=null;
     }
 }
 
+
 class LRUCache {
-    private Map<Integer,Node> map;
-    private int capacity;
-    private Node head;
-    private Node tail;
+    Map<Integer,Node> map;
+    int capacity;
+    Node head;
+    Node tail;
     public LRUCache(int capacity) {
-        map = new HashMap<>();
         this.capacity = capacity;
+        map = new HashMap<>();
         head = new Node(-1,-1);
         tail = new Node(-1,-1);
         head.next = tail;
@@ -27,43 +28,42 @@ class LRUCache {
     
     public int get(int key) {
         if(!map.containsKey(key)) return -1;
-        Node curr = map.get(key);
-        int ans = curr.value;
-        deleteNode(curr);
-        insertAfterHead(curr);
-    return ans;
+        Node currNode = map.get(key);
+        int ans = currNode.value;
+        deleteNode(currNode);
+        insertAfterHead(currNode);
+        return ans;
     }
     
     public void put(int key, int value) {
         if(map.containsKey(key)){
-            Node curr = map.get(key);
-            curr.value = value;
-            deleteNode(curr);
-            insertAfterHead(curr);
+            Node currNode = map.get(key);
+            currNode.value = value;
+            deleteNode(currNode);
+            insertAfterHead(currNode);
+            return;
         }
-        else{
-            if(capacity==map.size()){
-                Node toBeDeleted = tail.prev;
-                int toBeDeletedKey = toBeDeleted.key;
-                map.remove(toBeDeletedKey);
-                deleteNode(toBeDeleted);
-            }
-            Node newNode = new Node(key, value);
-            insertAfterHead(newNode);
-            map.put(key, newNode);
+        if(capacity == map.size()){
+            Node toBeDeleted = tail.prev;
+            int toBeDeletedKey = toBeDeleted.key;
+            map.remove(toBeDeletedKey);
+            deleteNode(toBeDeleted);
         }
-    }
-    public void insertAfterHead(Node node){
-        Node next = head.next;
-        head.next = node;
-        node.prev = head;
-        node.next = next;
-        next.prev=node; 
+        Node newNode = new Node(key,value);
+        insertAfterHead(newNode);
+        map.put(key, newNode);
     }
     public void deleteNode(Node node){
         Node prev = node.prev;
         Node next = node.next;
-        prev.next = next;
-        next.prev = prev;
+        prev.next=next;
+        next.prev=prev;
+    }
+    public void insertAfterHead(Node node){
+        Node next = head.next;
+        head.next=node;
+        node.next=next;
+        node.prev=head;
+        next.prev=node;
     }
 }
