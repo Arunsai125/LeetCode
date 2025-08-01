a7d31a8286c01cd6d229d45068544d3930208868
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-07-31 21:48:13 -0400

    Time: 0 ms (100%), Space: 44.5 MB (44.65%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        int k = recursion(root);
+    return k!=-1;        
+    }
+    public int recursion(TreeNode root){
+        if(root==null) return 0;
+        int left = recursion(root.left);
+            if(left==-1) return -1;
+        int right = recursion(root.right);
+            if(right==-1) return -1;
+        if(Math.abs(right-left)>1) return -1;
+    return 1 + Math.max(left,right);
+    }
+}
