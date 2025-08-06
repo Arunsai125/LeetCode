c2903244f0e727ca3396f9fc1fc0d82c68d93af4
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-08-05 20:08:49 -0400

    Time: 0 ms (100%), Space: 41.5 MB (15.04%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,17 +1,14 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int dp[] = new int[n];
-        Arrays.fill(dp, -1);
-        return recursion(nums, dp, n-1);
-    }
-    public int recursion(int[] nums, int[] dp, int ind){
-        if(ind == 0) return nums[0];
-        if(ind == 1) return Math.max(nums[0],nums[1]);
-        if(dp[ind]!=-1) return dp[ind];
-        int ans = Integer.MIN_VALUE;
-        if(ind-2>=0) ans = Math.max(ans, nums[ind] + recursion(nums, dp, ind-2));
-        ans = Math.max(ans, recursion(nums, dp, ind-1));
-    return dp[ind] = ans;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        if(n==1) return dp[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        if(n<=2) return dp[n-1];
+        for(int i=2;i<n;i++){
+            dp[i] = Math.max(dp[i-1], nums[i] + dp[i-2]);
+        }
+    return dp[n-1];
     }
 }
