8699ff545357d24e74495aaadd60a56f532e24ec
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-08-07 18:07:05 -0400

    Time: 0 ms (100%), Space: 40.2 MB (89.11%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,15 +1,19 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        for(int[] arr : dp) Arrays.fill(arr, -1);
-    return recursion(dp, m-1,n-1);
-    }
-    public int recursion(int[][] dp, int i, int j){
-        if(i==0 && j==0) return 1;
-        if(i<0 || j<0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        int left = recursion(dp, i, j-1);
-        int top = recursion(dp, i-1, j);
-    return dp[i][j] = top + left;
+        dp[0][0]=1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) continue;
+                else{
+                    int left=0;
+                    int up=0;
+                   if(j-1>=0)  left = dp[i][j-1];
+                   if(i-1>=0)  up = dp[i-1][j];
+                    dp[i][j] = left + up;
+                }
+            }
+        }
+    return dp[m-1][n-1];
     }
 }
