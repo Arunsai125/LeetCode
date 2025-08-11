dae3724f425d0169113d12b6271c24209d7881cc
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-08-11 00:01:00 -0400

    Time: 5 ms (26.89%), Space: 48 MB (21.74%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -3,16 +3,18 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         int[][] dp = new int[m][n];
-        for(int[] arr : dp) Arrays.fill(arr, -1);
-    return recursion(grid, dp, m-1, n-1);
-    }
-    public int recursion(int[][] grid, int[][] dp, int i, int j){
-        if(i==0 && j==0) return grid[0][0];
-        if(dp[i][j]!=-1) return dp[i][j];
-        int path1 = Integer.MAX_VALUE;
-        int path2 = Integer.MAX_VALUE;
-        if(j-1>=0) path1 = grid[i][j] + recursion(grid, dp, i, j-1);
-        if(i-1>=0) path2 = grid[i][j] + recursion(grid, dp, i-1, j);
-        return dp[i][j] = Math.min(path1, path2);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) dp[0][0] = grid[0][0];
+                else{
+                    int left = Integer.MAX_VALUE;
+                    int top = Integer.MAX_VALUE;
+                    if(j-1>=0) left = grid[i][j] + dp[i][j-1];
+                    if(i-1>=0) top = grid[i][j] + dp[i-1][j];
+                dp[i][j] = Math.min(left, top);
+                }
+            }
+        }
+    return dp[m-1][n-1];
     }
 }
