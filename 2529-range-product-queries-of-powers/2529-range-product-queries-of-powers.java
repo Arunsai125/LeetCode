1f57b10b243981bf5aa3978203fcd2b2ee2394c0
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-08-10 23:31:55 -0400

    Time: 19 ms (78.49%), Space: 92.8 MB (38.71%) - LeetHub

--- 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.java
+++ 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.java
@@ -1,24 +1,21 @@
 class Solution {
     private static final int modulo = 1_000_000_007;
-    public int[] productQueries(int n1, int[][] queries) {
-        int n = queries.length;
-        int[] ans = new int[n];
+    public int[] productQueries(int n, int[][] queries) {
+        int len = queries.length;
+        int[] ans = new int[len];
         List<Integer> powers = new ArrayList<>();
-        int num=n1;
-        int i1=0;
-        while(num!=0){
-            if((num&1) !=0 ) powers.add((int)Math.pow(2,i1));
-            i1++;
-            num = num>>1;
+        String str = Integer.toBinaryString(n);
+        int pow=0;
+        for(int i=str.length()-1;i>=0;i--){
+            if(str.charAt(i)!= '0') powers.add(1<<pow);
+            pow++;
         }
-        for(int i=0;i<n;i++){
+        for(int i=0;i<len;i++){
+            long product=1;
             int start = queries[i][0];
             int end = queries[i][1];
-            long product=1;
-            for(int j=start;j<=end;j++){
-                product = (product * powers.get(j)) % modulo;
-            }
-        ans[i] = (int)product;
+            for(int j=start;j<=end;j++) product = (product*powers.get(j))%modulo;
+            ans[i] = (int) product;
         }
     return ans;
     }
