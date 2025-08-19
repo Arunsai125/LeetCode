eaab5f861ed54625d5f084f8ab6e3babe898d888
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-08-18 20:01:41 -0400

    Time: 7 ms (19.85%), Space: 45 MB (5.03%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.java
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.java
@@ -4,13 +4,14 @@ class Solution {
         for(int i=0;i<num.length()-2;i++){
             StringBuilder sb = new StringBuilder();
             for(int j=i;j<i+3;j++){
-                sb.append(num.charAt(j));
+                if(sb.length()==0) sb.append(num.charAt(j));
+                else if(sb.charAt(sb.length()-1) != num.charAt(j)) break;
+                else sb.append(num.charAt(j));
             }
-            if(sb.charAt(0)==sb.charAt(1) && sb.charAt(1)==sb.charAt(2))
-                ans = Math.max(ans, Integer.valueOf(sb.toString()));
+            if(sb.length()==3) ans = Math.max(ans, Integer.valueOf(sb.toString()));
         }
-        if(ans==-1) return "";
         if(ans==0) return "000";
+        if(ans==-1) return "";
     return String.valueOf(ans);
     }
 }
