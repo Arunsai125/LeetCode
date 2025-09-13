8eb03e97c489d2f4a7f0a74db843df339dd341cc
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-09-13 10:58:56 -0400

    Time: 1 ms (100%), Space: 42.5 MB (64.55%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
@@ -1,18 +1,15 @@
 class Solution {
     public int maxFreqSum(String s) {
-        int[] vowels = new int[26];
-        int[] consonents = new int[26];
+        int[] freq = new int[26];
         for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') vowels[ch-'a']++;
-            else consonents[ch-'a'] ++;
+            freq[s.charAt(i)-'a']++;
         }
-        int vowelCount = 0;
-        int consonentCount = 0;
+        int maxVowelCount=0;
+        int maxConsonentCount=0;
         for(int i=0;i<26;i++){
-            vowelCount = Math.max(vowelCount, vowels[i]);
-            consonentCount = Math.max(consonentCount, consonents[i]);
+            if(i==0 || i==4 || i==8 || i==14 ||i==20)  maxVowelCount = Math.max( maxVowelCount, freq[i]);
+            else maxConsonentCount = Math.max(maxConsonentCount, freq[i]);
         }
-    return vowelCount + consonentCount;
+    return maxConsonentCount + maxVowelCount;
     }
 }
