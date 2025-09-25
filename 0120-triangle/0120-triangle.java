e007be310bda8c030b32f2e3c0e984e4865a5679
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-09-25 09:21:05 -0400

    Time: 5 ms (34.96%), Space: 45.7 MB (30.46%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,25 +1,29 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.get(triangle.size()-1).size();
-        int c = triangle.size()-1;
-        int[][] dp = new int[n][n];
+        int dp[][] = new int[n][n];
         dp[0][0] = triangle.get(0).get(0);
         if(triangle.size()==1) return dp[0][0];
-        for(int i=1;i<triangle.size();i++){
-            for(int j=0;j<triangle.get(i).size();j++){
-                if(j==0) dp[i][j] = triangle.get(i).get(j) + dp[i-1][j];
-                else if(j==triangle.get(i).size()-1) dp[i][j] = triangle.get(i).get(j) + dp[i-1][j-1];
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i+1;j++){
+                if(j==0)
+                    dp[i][j] = triangle.get(i).get(j) + dp[i-1][j];
+                else if(j==i){
+                    dp[i][j] = triangle.get(i).get(j) + dp[i-1][j-1];
+                }
                 else{
                     int path1 = Integer.MAX_VALUE;
                     int path2 = Integer.MAX_VALUE;
-                    path1 = triangle.get(i).get(j) + dp[i-1][j-1];
-                    path2 = triangle.get(i).get(j) + dp[i-1][j];
+                    path1 = triangle.get(i).get(j) + dp[i-1][j];
+                    path2 =  triangle.get(i).get(j) + dp[i-1][j-1];
                     dp[i][j] = Math.min(path1, path2);
                 }
             }
         }
-        int ans = dp[c][0];
-        for(int i=1;i<n;i++) ans = Math.min(ans, dp[c][i]);
+        int ans = dp[n-1][0];
+        for(int i=1;i<n;i++){
+            ans = Math.min(ans, dp[n-1][i]);
+        }
     return ans;
     }
 }
