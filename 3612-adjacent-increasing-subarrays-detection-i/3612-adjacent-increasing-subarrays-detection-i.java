42edbbb4f58869ee079a230cd364547e547494c3
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-10-15 20:59:19 -0400

    Time: 1 ms (100%), Space: 44.3 MB (91.97%) - LeetHub

--- 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.java
+++ 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.java
@@ -1,15 +1,17 @@
 class Solution {
     public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
-        int knew = k - 1;
-        if (knew == 0) return true;
-        for (int j = k + 1; j < nums.size(); j++) {
-            if (nums.get(j) > nums.get(j - 1) && nums.get(j - k) > nums.get(j - k - 1)) {
-                knew--;
-            } else {
-                knew = k - 1;
+        int ans = 1;
+        int curr=1;
+        int prev=1;
+        for(int i=1;i<nums.size();i++){
+            if(nums.get(i) > nums.get(i-1)) curr++;
+            else{
+                prev = curr;
+                curr = 1;
             }
-            if (knew == 0) return true;
+            ans = Math.max(ans, curr/2);
+            ans = Math.max(ans, Math.min(prev, curr));
         }
-        return false;
+    return ans>=k;
     }
 }
