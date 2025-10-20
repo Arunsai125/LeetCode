eb96a6cfe92c74a92e5251035b93ad69620c17d2
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-10-19 22:07:49 -0400

    Time: 7 ms (78.05%), Space: 45.5 MB (6.63%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.java
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.java
@@ -9,25 +9,25 @@ class Pair{
 
 class Solution {
     public int swimInWater(int[][] grid) {
-        int start=grid[0][0];
-        int end = (grid.length * grid.length)-1;
         int ans=grid[0][0];
+        int start=grid[0][0];
+        int end = (grid.length * grid.length) - 1;
         while(start<=end){
-            int mid = (start + end)/2;
-            if(isReachable(grid,mid)){
-                ans=mid;
-                end=mid-1;
+            int mid = (start+end) / 2;
+            if(isPossible(grid, mid, grid.length)){
+                ans = mid;
+                end = mid-1;
             }
-            else start = mid + 1;
+            else start = mid+1;
         }
-    return ans;
+        return ans;
     }
-    public boolean isReachable(int[][] nums, int target){
-        int n =nums.length;
+
+    public boolean isPossible(int[][] grid, int curr, int n){
         Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(0,0));
         boolean[][] visited = new boolean[n][n];
         visited[0][0] = true;
+        q.offer(new Pair(0,0));
         int[] row = {-1,0,1,0};
         int[] col = {0,1,0,-1};
         while(!q.isEmpty()){
@@ -36,15 +36,13 @@ class Solution {
                 Pair temp = q.poll();
                 int r = temp.r;
                 int c = temp.c;
-                if(r==n-1 && c==n-1){
-                    return true;
-                }
+                if(r==n-1 && c==n-1) return true;
                 for(int j=0;j<4;j++){
-                    int newRow = r + row[j];
-                    int newCol = c + col[j];
-                    if(newRow >=0 && newRow<n && newCol>=0 && newCol<n && !visited[newRow][newCol] && nums[newRow][newCol]<=target){
-                        visited[newRow][newCol] = true;
-                        q.add(new Pair(newRow,newCol));
+                    int newR = r + row[j];
+                    int newC = c + col[j];
+                    if(newR>=0 && newR < n && newC>=0 && newC<n && visited[newR][newC]==false && grid[newR][newC]<=curr){
+                        visited[newR][newC] = true;
+                        q.offer(new Pair(newR,newC));
                     }
                 }
             }
