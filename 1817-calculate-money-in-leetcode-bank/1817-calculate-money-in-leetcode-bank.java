f17fd9fb643e87c9d45c26c404632d560d44c58e
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-10-24 20:25:16 -0400

    Time: 2 ms (17.17%), Space: 40.6 MB (44.44%) - LeetHub

---
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int totalMoney(int n) {
+        int ans = 0;
+        int extraValue = 0;
+        for(int i=1;i<=n;i++){
+            ans = ans + (i%7 == 0 ? 7 : i%7) + extraValue;
+            if(i/7 > extraValue) extraValue++;
+        }
+    return ans;
+    }
+}
