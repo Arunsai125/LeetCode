30ec19cafaa57629e7b6b41f49b74b9c91eb024d
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-11-02 16:48:17 -0500

    Time: 24 ms (63.45%), Space: 147.2 MB (5.52%) - LeetHub

--- 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.java
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.java
@@ -1,26 +1,30 @@
 class Solution {
     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
-        int[][] state = new int[m][n];
-        for (int[] w : walls) state[w[0]][w[1]] = 1;
-        for (int[] g : guards) state[g[0]][g[1]] = 1;
-        int[] dx = {-1, 0, 1, 0};
-        int[] dy = {0, 1, 0, -1};
-        for (int[] g : guards) {
-            int gx = g[0], gy = g[1];
-            for (int d = 0; d < 4; d++) {
-                int x = gx + dx[d], y = gy + dy[d];
-                while (x >= 0 && x < m && y >= 0 && y < n) {
-                    if (state[x][y] == 1) break;
-                    state[x][y] = 2;
-                    x += dx[d];
-                    y += dy[d];
+        int ans = 0;
+        int[][] visited = new int[m][n];
+        for(int[] guard : guards) visited[guard[0]][guard[1]] = 1;
+        for(int[] wall : walls) visited[wall[0]][wall[1]] = 1;
+        int dx[] = {-1,0,1,0};
+        int dy[] = {0,1,0,-1};
+        for(int[] guard : guards){
+            int x = guard[0];
+            int y = guard[1];
+            for(int i=0;i<4;i++){
+                int X = x + dx[i];
+                int Y = y + dy[i];
+                while(X>=0 && X<m && Y>=0 && Y<n){
+                    if(visited[X][Y] == 1) break;
+                    visited[X][Y] = 2;
+                    X += dx[i];
+                    Y += dy[i];
                 }
             }
         }
-        int ans = 0;
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                if (state[i][j] == 0) ans++;
-        return ans;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(visited[i][j] == 0) ans++;
+            }
+        }
+    return ans;
     }
 }
