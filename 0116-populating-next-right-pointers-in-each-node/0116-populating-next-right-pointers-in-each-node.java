ee4c87d4da24d64b6929c4430964d98fd656b80f
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2025-12-09 20:06:33 -0500

    Time: 2 ms (50.65%), Space: 46.4 MB (40.47%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -1,46 +1,21 @@
-
-// class Node {
-//     public int val;
-//     public Node left;
-//     public Node right;
-//     public Node next;
-
-//     public Node() {}
-    
-//     public Node(int _val) {
-//         val = _val;
-//     }
-
-//     public Node(int _val, Node _left, Node _right, Node _next) {
-//         val = _val;
-//         left = _left;
-//         right = _right;
-//         next = _next;
-//     }
-// };
-
 class Solution {
     public Node connect(Node root) {
         if(root==null) return root;
         Queue<Node> q = new LinkedList<>();
-        List<List<Node>> list = new ArrayList<>(); 
-        root.next = null;
         q.add(root);
         while(!q.isEmpty()){
+            Node prev = null;
             int k = q.size();
-            List<Node> tempList = new ArrayList<>();
             for(int i=0;i<k;i++){
-                Node temp = q.poll();
-                if(temp.left!=null) { q.add(temp.left); tempList.add(temp.left); }
-                if(temp.right!=null) { q.add(temp.right); tempList.add(temp.right); }
-            }
-            if(tempList.size()>0) list.add(tempList);
-        }
-        for(List<Node> al : list){
-                for(int i=0;i<al.size()-1;i++){
-                    al.get(i).next = al.get(i+1);
+                Node curr = q.poll();
+                if(prev != null){
+                    prev.next = curr;
                 }
-                al.get(al.size()-1).next = null;
+                prev = curr;
+                if(curr.left!=null) q.add(curr.left);
+                if(curr.right!=null) q.add(curr.right);
+            }
+            prev.next = null;
         }
     return root;
     }
