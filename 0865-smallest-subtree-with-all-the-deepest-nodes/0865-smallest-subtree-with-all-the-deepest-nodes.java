b8b7a756bc1e51e854752fc2af6b9b03ad7cbaa9
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2026-01-09 16:18:40 -0500

    Time: 0 ms (100%), Space: 43.4 MB (68.65%) - LeetHub

--- 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.java
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.java
@@ -1,24 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public TreeNode subtreeWithAllDeepest(TreeNode root) {
-        int maximumDepth = treeDepth(root);
-        return findAncestors(root, 1, maximumDepth);
+        int height = findHeight(root);
+        return findSubtree(root, height, 1);
     }
-
-    public TreeNode findAncestors(TreeNode root, int depth, int maximumDepth){
-        if(maximumDepth == depth || root == null)
-            return root;
-        TreeNode left = findAncestors(root.left, depth + 1, maximumDepth);
-        TreeNode right = findAncestors(root.right, depth + 1, maximumDepth);
-        if(left != null && right != null) return root;
-        else if(right == null) return left;
-        return right;
+    public int findHeight(TreeNode root){
+        if(root==null) return 0;
+        int leftHeight = findHeight(root.left);
+        int rightHeight = findHeight(root.right);
+        return 1 + Math.max(leftHeight, rightHeight);
     }
-
-    public int treeDepth(TreeNode root){
-        if(root == null)
-            return 0;
-        int left = treeDepth(root.left);
-        int right = treeDepth(root.right);
-        return 1 + Math.max(left, right);
+    public TreeNode findSubtree(TreeNode root, int maxHeight, int height){
+        if(root==null || height==maxHeight) return root;
+        TreeNode left = findSubtree(root.left, maxHeight, height+1);
+        TreeNode right = findSubtree(root.right, maxHeight, height+1);
+        if(left==null) return right;
+        else if(right==null) return left;
+    return root;
     }
 }
