4aa0645d5567ef9f484df1e9287419bdf8659684
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2026-01-09 16:26:27 -0500

    Time: 9 ms (15.38%), Space: 46.6 MB (47.17%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -1,22 +1,21 @@
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-       List<TreeNode> list1 = new ArrayList<>();
-       List<TreeNode> list2 = new ArrayList<>();
-       findPath(root, p, list1);
-       findPath(root, q, list2);
-       int n = Math.min(list1.size(), list2.size());
-       TreeNode ans = new TreeNode(-1);
-       for(int i=0;i<n;i++){
-            if(list1.get(i) == list2.get(i)) ans = list1.get(i);
-            else break;
-       }
-    return ans;
+        List<TreeNode> path1 = new ArrayList<>();
+        List<TreeNode> path2 = new ArrayList<>();
+        findPath(root, q, path1); findPath(root, p, path2);
+        int n = Math.min(path1.size(), path2.size());
+        TreeNode ans = root;
+        for(int i=0;i<n;i++){
+            if(path1.get(i)==path2.get(i)) ans = path1.get(i);
+           else break;
+        }
+    return ans;        
     }
     public boolean findPath(TreeNode root, TreeNode target, List<TreeNode> ans){
-        if(root == null) return false;
+        if(root==null) return false;
         ans.add(root);
-        if(root == target) return true;
-        if(findPath(root.left,target,ans) || findPath(root.right, target, ans)) return true;
+        if(root==target) return true;
+        if(findPath(root.left,target,ans) || findPath(root.right,target,ans)) return true;
         ans.remove(ans.size()-1);
     return false;
     }
