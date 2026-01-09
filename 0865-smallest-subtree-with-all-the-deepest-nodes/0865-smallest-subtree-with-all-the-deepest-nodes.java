7fba8ae177ed3c47a7bee1265f372f90d8bb041e
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2026-01-08 20:08:14 -0500

    Time: 0 ms (100%), Space: 43.5 MB (68.02%) - LeetHub

---
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.java
@@ -0,0 +1,24 @@
+class Solution {
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        int maximumDepth = treeDepth(root);
+        return findAncestors(root, 1, maximumDepth);
+    }
+
+    public TreeNode findAncestors(TreeNode root, int depth, int maximumDepth){
+        if(maximumDepth == depth || root == null)
+            return root;
+        TreeNode left = findAncestors(root.left, depth + 1, maximumDepth);
+        TreeNode right = findAncestors(root.right, depth + 1, maximumDepth);
+        if(left != null && right != null) return root;
+        else if(right == null) return left;
+        return right;
+    }
+
+    public int treeDepth(TreeNode root){
+        if(root == null)
+            return 0;
+        int left = treeDepth(root.left);
+        int right = treeDepth(root.right);
+        return 1 + Math.max(left, right);
+    }
+}
