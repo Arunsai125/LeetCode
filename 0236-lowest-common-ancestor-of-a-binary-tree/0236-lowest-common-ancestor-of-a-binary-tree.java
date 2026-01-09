b6b4145fcd86f4c5099d4720da2a0d7c9162d123
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2026-01-09 16:29:43 -0500

    Time: 7 ms (98.82%), Space: 46.9 MB (8.41%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -1,22 +1,11 @@
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        List<TreeNode> path1 = new ArrayList<>();
-        List<TreeNode> path2 = new ArrayList<>();
-        findPath(root, q, path1); findPath(root, p, path2);
-        int n = Math.min(path1.size(), path2.size());
-        TreeNode ans = root;
-        for(int i=0;i<n;i++){
-            if(path1.get(i)==path2.get(i)) ans = path1.get(i);
-           else break;
-        }
-    return ans;        
-    }
-    public boolean findPath(TreeNode root, TreeNode target, List<TreeNode> ans){
-        if(root==null) return false;
-        ans.add(root);
-        if(root==target) return true;
-        if(findPath(root.left,target,ans) || findPath(root.right,target,ans)) return true;
-        ans.remove(ans.size()-1);
-    return false;
+        if(root==null || root==p || root==q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left==null && right==null) return null;
+        else if(left==null) return right;
+        else if(right==null) return left;
+    return root; 
     }
 }
