416a16ba5e2b87227593e9d726cf6d56004bbb43
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2026-01-10 20:37:36 -0500

    Time: 63 ms (70.94%), Space: 78.4 MB (41.97%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,30 +1,23 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
+        Stack<Integer> st = new Stack<>();
         int n = heights.length;
-        int nse[] = new int[n];
-        int pse[] = new int[n];
-        findNSE(heights, nse, n);
-        findPSE(heights, pse, n);
         int ans = 0;
         for(int i=0;i<n;i++){
-            ans = Math.max(ans, (nse[i] - pse[i] - 1) * heights[i]);
+            while(!st.isEmpty() && heights[st.peek()] > heights[i]){
+                int element = heights[st.pop()];
+                int nse = i;
+                int pse = st.isEmpty() ? -1 : st.peek();
+                ans = Math.max(ans, element * (nse-pse-1));
+            }
+            st.push(i);
+        }
+        while(!st.isEmpty()){
+            int element = heights[st.pop()];
+            int nse = n;
+            int pse = st.isEmpty() ? -1 : st.peek();
+            ans = Math.max(ans, element * (nse-pse-1));
         }
     return ans;
-    }
-    public void findNSE(int[] heights, int[] nse, int n){
-        Stack<Integer> st = new Stack<>();
-        for(int i=n-1;i>=0;i--){
-            while(!st.isEmpty() && heights[st.peek()] >= heights[i]) st.pop();
-            nse[i] = st.isEmpty() ? n : st.peek();
-            st.push(i);
-        }
-    }
-    public void findPSE(int[] heights, int[] pse, int n){
-        Stack<Integer> st = new Stack<>();
-        for(int i=0;i<n;i++){
-            while(!st.isEmpty() && heights[st.peek()] >= heights[i]) st.pop();
-            pse[i] = st.isEmpty() ? -1 : st.peek();
-            st.push(i);
-        }
     }
 }
