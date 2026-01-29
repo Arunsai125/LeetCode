94a5e3270ca1ec50b3079b07a7afc28d969c305f
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2026-01-29 09:45:39 -0500

    Time: 75 ms (82.82%), Space: 273.5 MB (57.79%) - LeetHub

--- 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.java
+++ 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.java
@@ -1,38 +1,36 @@
 class Pair{
-    int distance;
+    int weight;
     int node;
-    public Pair(int distance, int node){
-        this.distance = distance;
-        this.node = node;
+    public Pair(int weight, int node){
+        this.weight=weight;
+        this.node=node;
     }
 }
 
+
 class Solution {
     public int minCost(int n, int[][] edges) {
-        List<List<Pair>> list = new ArrayList<>();
-        for(int i=0;i<n;i++) list.add(new ArrayList<Pair>());
+        List<List<Pair>> adjList = new ArrayList<>();
+        for(int i=0;i<n;i++) adjList.add(new ArrayList<Pair>());
         for(int i=0;i<edges.length;i++){
             int u = edges[i][0];
             int v = edges[i][1];
             int w = edges[i][2];
-            list.get(u).add(new Pair(w,v));
-            list.get(v).add(new Pair(2*w,u));
+            adjList.get(u).add(new Pair(w, v));
+            adjList.get(v).add(new Pair(2*w, u));
         }
         int[] distance = new int[n];
         Arrays.fill(distance, Integer.MAX_VALUE);
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.weight - b.weight);
         distance[0] = 0;
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.distance - b.distance);
         pq.add(new Pair(0,0));
         while(!pq.isEmpty()){
             Pair temp = pq.poll();
             if(temp.node == n-1) return distance[temp.node];
-            if (temp.distance != distance[temp.node]) continue;
-            for(Pair p : list.get(temp.node)){
-                int v = p.node;
-                int w = p.distance;
-                if(distance[temp.node] + w < distance[v]) {
-                    distance[v] = distance[temp.node] + w ;
-                    pq.add(new Pair(distance[v], v));
+            for(Pair adj : adjList.get(temp.node)){
+                if(distance[temp.node] + adj.weight < distance[adj.node]){
+                    distance[adj.node] = distance[temp.node] + adj.weight;
+                    pq.add(new Pair(distance[adj.node], adj.node));
                 }
             }
         }
