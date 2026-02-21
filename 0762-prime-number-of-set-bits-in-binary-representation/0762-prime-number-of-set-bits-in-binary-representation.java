3a0f553b94d5b7bd1dba1d61da13ded6a957a18a
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2026-02-20 19:54:04 -0500

    Time: 4 ms (87.21%), Space: 42.4 MB (44.84%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.java
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.java
@@ -1,34 +1,13 @@
 class Solution {
-    public int countPrimeSetBits(int left, int right) {
-        int n = Math.min(right, 10_00_001);
-        int[] array = new int[n+1];
-        sieveOfEratosthenes(array);
+    public int countPrimeSetBits(int L, int R) {
         int ans = 0;
-        for(int i=left;i<=right;i++){
-            String str = Integer.toBinaryString(i);
-            int setBits = noOfSetBits(str);
-            if(array[setBits] == 1) ans++;
-        }
-    return ans;
+        for (int x = L; x <= R; ++x)
+            if (isSmallPrime(Integer.bitCount(x)))
+                ans++;
+        return ans;
     }
-    public void sieveOfEratosthenes(int[] array){
-        Arrays.fill(array, 1);
-        int n = array.length;
-        array[0] = 0;
-        array[1] = 0;
-        for(int i=2; i*i <= n; i++){
-            if(array[i] == 1){
-                for(int j=i*i;j<=n;j+=i){
-                    if(j<n) array[j] = 0;
-                }
-            }
-        }
-    }
-    public int noOfSetBits(String str){
-        int ans=0;
-        for(int i=0;i<str.length();i++){
-            if(str.charAt(i) == '1') ans++;
-        }
-    return ans;
+    public boolean isSmallPrime(int x) {
+        return (x == 2 || x == 3 || x == 5 || x == 7 ||
+                x == 11 || x == 13 || x == 17 || x == 19);
     }
 }
