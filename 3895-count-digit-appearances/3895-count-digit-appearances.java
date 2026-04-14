a4180c2537d544044b02a79f7af9f9cc3b923d4b
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2026-04-14 18:51:33 -0400

    Time: 4 ms (98.28%), Space: 46.9 MB (85.41%) - LeetHub

--- 3895-count-digit-appearances/3895-count-digit-appearances.java
+++ 3895-count-digit-appearances/3895-count-digit-appearances.java
@@ -1,12 +1,17 @@
 class Solution {
     public int countDigitOccurrences(int[] nums, int digit) {
         int ans = 0;
-        char target = (char) ('0' + digit);
         for(int i : nums){
-            String str = String.valueOf(i);
-            for(char ch : str.toCharArray()){
-                if(ch == target) ans++;
-            }
+            ans += countOccurencesOfDigit(i,digit);
+        }
+    return ans;
+    }
+    public int countOccurencesOfDigit(int num, int digit){
+        int ans = 0;
+        while(num != 0){
+            int d = num%10;
+            if(d==digit) ans++;
+            num /= 10;
         }
     return ans;
     }
