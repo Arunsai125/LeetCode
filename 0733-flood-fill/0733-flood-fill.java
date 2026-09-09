06f38bb280529e59383c80069713b8f410043005
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2026-09-09 17:48:19 -0400

    Time: 1 ms (38.07%), Space: 46.9 MB (24.41%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,20 +1,22 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int originalColor = image[sr][sc];
-        if(image[sr][sc]==color) return image;
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{sr,sc});
+        int startingPixelColor = image[sr][sc];
+        if (startingPixelColor == color) return image;
+        int m = image.length;
+        int n = image[0].length;
         image[sr][sc] = color;
         int[] dx = {-1,0,1,0};
         int[] dy = {0,1,0,-1};
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{sr,sc});
         while(!q.isEmpty()){
             int k = q.size();
             for(int i=0;i<k;i++){
-                int[] temp = q.poll();
+                int[] top = q.poll();
                 for(int j=0;j<4;j++){
-                    int newX = temp[0] + dx[j];
-                    int newY = temp[1] + dy[j];
-                    if(newX>=0 && newX<image.length && newY>=0 && newY<image[0].length && image[newX][newY] == originalColor){
+                    int newX = top[0] + dx[j];
+                    int newY = top[1] + dy[j];
+                    if(newX>=0 && newX<m && newY>=0 && newY<n && image[newX][newY]==startingPixelColor){
                         image[newX][newY] = color;
                         q.add(new int[]{newX,newY});
                     }
