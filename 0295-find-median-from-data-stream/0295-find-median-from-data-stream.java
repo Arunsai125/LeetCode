98d2a7a2fc96726dda3d13f92a1a87dd2817d7cf
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2026-09-16 08:21:12 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,21 +1,20 @@
 class MedianFinder {
-    private PriorityQueue<Integer> minHeap;
-    private PriorityQueue<Integer> maxHeap;
+    PriorityQueue<Integer> minHeap;
+    PriorityQueue<Integer> maxHeap;
     public MedianFinder() {
-        minHeap = new PriorityQueue<>((a,b) -> a-b);
-        maxHeap = new PriorityQueue<>((a,b) -> b-a);
+        minHeap = new PriorityQueue<>();
+        maxHeap = new PriorityQueue<>((a,b)->b-a);
     }
     
     public void addNum(int num) {
-        if(maxHeap.isEmpty() || num <= maxHeap.peek()) maxHeap.offer(num);
-        else minHeap.offer(num);
-        if(maxHeap.size() > minHeap.size() + 1) minHeap.offer(maxHeap.poll());
+        if(maxHeap.isEmpty() || num < maxHeap.peek()) maxHeap.offer(num);
+        else minHeap.add(num);
         if(minHeap.size() > maxHeap.size()) maxHeap.offer(minHeap.poll());
     }
     
     public double findMedian() {
-        if(minHeap.size() == 0 && maxHeap.size() == 0) return 0.0;
-        if(minHeap.size() == maxHeap.size()) return (double) (maxHeap.peek() + minHeap.peek())/2;
+        if(maxHeap.size() == minHeap.size()) return (double) (maxHeap.peek() + minHeap.peek())/2.0;
         return maxHeap.peek();
     }
 }
+
