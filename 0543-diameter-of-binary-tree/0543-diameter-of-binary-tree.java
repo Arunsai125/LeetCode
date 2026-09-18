9a301d71f51a8127b8539a77c1ab5fe25a9ff5e0
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2026-09-17 22:11:01 -0400

    Time: 195 ms (5.85%), Space: 46.8 MB (93.59%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -1,14 +1,21 @@
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
-        int ans[] = {0};
-        recursion(root, ans);
-    return ans[0];
+        int ans = 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int k = q.size();
+            for(int i=0;i<k;i++){
+                TreeNode top = q.poll();
+                ans = Math.max(ans, findHeight(top.left) + findHeight(top.right));
+                if(top.left!=null) q.add(top.left);
+                if(top.right!=null) q.add(top.right);
+            }
+        }
+    return ans;
     }
-    public int recursion(TreeNode root, int[] ans){
+    public int findHeight(TreeNode root){
         if(root==null) return 0;
-        int left = recursion(root.left, ans);
-        int right = recursion(root.right, ans);
-        ans[0] = Math.max(ans[0], left + right);
-        return 1 + Math.max(left,right);
+    return 1 + Math.max(findHeight(root.left),findHeight(root.right));
     }
 }
