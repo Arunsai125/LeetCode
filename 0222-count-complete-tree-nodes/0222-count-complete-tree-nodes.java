8f10f76debbf0b398ce14d7e407d494ddc53ae33
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2026-09-18 10:09:19 -0400

    Time: 0 ms (100%), Space: 49.7 MB (10.94%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -1,50 +1,27 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public int countNodes(TreeNode root) {
-        if (root == null)
-            return 0;
-
-        int leftHeight = getLeftheight(root);
-        int rightHeight = getRightheight(root);
-
-        if (leftHeight == rightHeight) {
-            return (1 << leftHeight) - 1;
-        }
-
-        return 1 + countNodes(root.left) + countNodes(root.right);
+        if(root==null) return 0;
+        int leftHeight = findLeftHeight(root);
+        int rightHeight = findRightHeight(root);
+        if(leftHeight == rightHeight) return (1<<leftHeight) - 1;
+    return 1 + countNodes(root.left) + countNodes(root.right);        
     }
-
-    private int getLeftheight(TreeNode node) {
-        int height = 0;
-        while (node != null) {
-            height++;
-            node = node.left;
+    public int findLeftHeight(TreeNode root){
+        if(root==null) return 0;
+        int ans = 0;
+        while(root!=null){
+            ans++;
+            root = root.left;
         }
-
-        return height;
+    return ans;
     }
-
-    private int getRightheight(TreeNode node) {
-        int height = 0;
-        while (node != null) {
-            height++;
-            node = node.right;
+    public int findRightHeight(TreeNode root){
+        if(root==null) return 0;
+        int ans=0;
+        while(root!=null){
+            ans++;
+            root=root.right;
         }
-
-        return height;
+    return ans;
     }
 }
