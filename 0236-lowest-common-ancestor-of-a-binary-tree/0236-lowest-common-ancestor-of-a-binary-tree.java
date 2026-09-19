721131b5d5fe7f12cb33e238e761a887d22be504
ArunSaiNarla <85918659+Arunsai125@users.noreply.github.com>
2026-09-19 11:09:08 -0400

    Time: 25 ms (8.42%), Space: 67.8 MB (97.25%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -1,11 +1,22 @@
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root==null || root==p || root==q) return root;
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if(left==null && right==null) return null;
-        else if(left==null) return right;
-        else if(right==null) return left;
-    return root; 
+        List<TreeNode> path1 = new ArrayList<>();
+        findPath(root, p, path1);
+        List<TreeNode> path2 = new ArrayList<>();;
+        findPath(root, q, path2);
+        TreeNode ans = null;
+        for(int i=0;i<Math.min(path1.size(), path2.size());i++){
+            if(path1.get(i) == path2.get(i))
+                ans = path1.get(i);
+        }
+    return ans;
+    }
+    public boolean findPath(TreeNode root, TreeNode target, List<TreeNode> path){
+        if(root==null) return false;
+        path.add(root);
+        if(root==target) return true;
+        if(findPath(root.left,target,path) || findPath(root.right, target, path)) return true;
+        path.remove(path.size()-1);
+    return false;
     }
 }
